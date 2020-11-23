0e60126941fcbd81ee71101baca4b80e7440765b
alvarodantas1997@hotmail.com <lvaro Dantas>
2020-11-23 20:45:06 -0300

    Models domain added

---
+++ QuickBuy.Domain/Entities/Request.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Domain.Entities
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int UserId { get; set; }
+        public ICollection<RequestItem> RequestItem { get; set; }
+    }
+}
---
+++ QuickBuy.Domain/Entities/RequestItem.cs
@@ -0,0 +1,9 @@
+﻿namespace QuickBuy.Domain.Entities
+{
+    public class RequestItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
